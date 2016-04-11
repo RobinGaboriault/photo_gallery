589c24daafa50f795882411067921b61f8c5e087
Robin <robingaboriault@hotmail.ca>
2016-04-11 18:42:52 -0400

    Worked on making the site responsive, added some basic jquery animation.

---
+++ js/main.js
@@ -0,0 +1,82 @@
+
+
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+// Add image to overlay
+$overlay.append($image);
+
+//Add caption to overlay
+$overlay.append($caption);
+
+//Add overlay
+$("body").append($overlay);
+
+
+
+
+// Click event on a link to an image 
+$("#photo_gallery a").click(function(event){
+	event.preventDefault();
+	var imageLocation = $(this).attr("href");
+	// Update overlay with the image linked 
+	$image.attr("src", imageLocation);
+
+	// Show overlay
+	$overlay.show();
+
+	//Get child alt attribute and set caption
+	var captionText = $(this).children("img").attr("alt");
+	$caption.text(captionText);
+
+
+});
+
+
+// Hide overlay when clicked
+$overlay.click(function(){
+
+	$overlay.hide();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
